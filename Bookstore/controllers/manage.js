82d766f05ac6ae810e1dc4d78dcd89b5dbbe4569
devrolet <chetdhill@gmail.com>
2017-10-25 16:35:03 -0400

    Add Book Functionality

--- Bookstore/controllers/manage.js
+++ Bookstore/controllers/manage.js
@@ -1,12 +1,38 @@
 'use strict';
 
+var Book = require('../models/bookModel');
+var Category = require('../models/categoryModel');
+
 module.exports = function(router) {
 	router.get('/', function(req, res) {
 		res.render('manage/index');
 	});
 
 	router.get('/books', function(req, res) {
-		res.render('manage/books/index');
+		Book.find({}, function(err, books){
+        	if(err){
+        		console.log(err);
+        	}
+
+        	var model = {
+        		books: books
+        	}
+        	res.render('manage/books/index', model);
+        });
+	});
+
+	router.get('/books/add', function(req, res){
+		Category.find({}, function(err, categories) {
+			if(err){
+				console.log(err);
+			}
+
+			var model = {
+				categories: categories
+			}
+
+			res.render('manage/books/add', model);
+		});
 	});
 
 	router.get('/categories', function(req, res) {

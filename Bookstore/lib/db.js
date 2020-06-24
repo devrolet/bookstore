51481d26c24f9557312b235e56cbd7fdae2e00a6
devrolet <chetdhill@gmail.com>
2020-06-24 10:20:46 -0400

    Update Bookstore App - Add Category

--- Bookstore/lib/db.js
+++ Bookstore/lib/db.js
@@ -5,7 +5,7 @@ var mongoose = require('mongoose');
 var db = function(){
 	return{
 		config: function(conf){
-			mongoose.connect('mongodb://localhost/bookstore');
+			mongoose.connect('mongodb://localhost/bookstore', { useNewUrlParser: true, useUnifiedTopology: true });
 			var db = mongoose.connection;
 			db.on('error', console.error.bind(console, 'Connection Error'));
 			db.once('open', function(){
